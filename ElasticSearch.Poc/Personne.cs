974eecacc4cf7623b85dd1fb382ef02fa8bebd1e
Nadege <nrouelle@outlook.fr>
2017-12-05 15:42:27 +0100

    Add NEST package and first query to select personne

---
+++ ElasticSearch.Poc/Personne.cs
@@ -0,0 +1,25 @@
+﻿namespace ElasticSearch.Poc
+{
+    internal class Personne
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Nom
+        /// </summary>
+        public string Nom { get; set; }
+        /// <summary>
+        /// Adresse
+        /// </summary>
+        public string Adresse { get; set; }
+        /// <summary>
+        /// Code Postal
+        /// </summary>
+        public string CodePostal { get; set; }
+
+        public string Ville { get; set; }
+    }
+}
--- ElasticSearch.Poc/Program.cs
+++ ElasticSearch.Poc/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Elasticsearch.Net;
+using Nest;
 
 namespace ElasticSearch.Poc
 {
@@ -11,14 +7,20 @@ namespace ElasticSearch.Poc
     {
         static void Main(string[] args)
         {
-            var urlElasticSearch = "";
-            string username = "nrouelle";
-            string password = "password";
+            const string urlElasticSearch = "http://server:port";
 
-            var settings = new ConnectionConfiguration(new Uri(urlElasticSearch))
-                .RequestTimeout(TimeSpan.FromMinutes(2));
-            settings.BasicAuthentication(username, password);
-            var client = new ElasticLowLevelClient(settings);
+            var settings = new ConnectionSettings(new Uri(urlElasticSearch))
+                .RequestTimeout(TimeSpan.FromMinutes(2)).DefaultIndex("index");
+
+            var elasticClient = new ElasticClient(settings);
+
+            var searchResponse = elasticClient.Search<Personne>(s => s.From(0).Size(20)
+                .Query(q => q
+                    .Match(m => m
+                        .Field(f => f.Id)
+                        .Query("1"))));
+
+            var personne = searchResponse.Documents;
         }
     }
 }
