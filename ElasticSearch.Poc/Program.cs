6bf98c514882f500a083c58252f308a8fb0966ad
Nadege <nrouelle@outlook.fr>
2017-12-05 10:09:28 +0100

    Init poc

---
+++ ElasticSearch.Poc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+
+namespace ElasticSearch.Poc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var urlElasticSearch = "";
+            string username = "nrouelle";
+            string password = "password";
+
+            var settings = new ConnectionConfiguration(new Uri(urlElasticSearch))
+                .RequestTimeout(TimeSpan.FromMinutes(2));
+            settings.BasicAuthentication(username, password);
+            var client = new ElasticLowLevelClient(settings);
+        }
+    }
+}
